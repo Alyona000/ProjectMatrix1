499b5ec62d29f0dca9ea0a057fed1d9ebde62a67
Alyona000 <153160427+Alyona000@users.noreply.github.com>
2024-03-04 21:01:38 +0800

    Version4

--- Functions.cpp
+++ Functions.cpp
@@ -2,7 +2,7 @@
 #include <cmath>
 using namespace std;
 // Функция ввода матрицы
-void Input_Matrix(int Lines, int Columns, int Matrix[20][20]) 
+void Input_Matrix(int Lines, int Columns, int Matrix[20][20])
 {
 	for (int i = 0; i < Lines; i++)
 	{
@@ -88,7 +88,8 @@ int Find_Det(int** Matrix, int Size)
  
 int main() 
 {
-	int Variant;
+	int Variant = 1;
+    while(Variant!=0) {
 	cout << "1. Quadratic equation" << endl;
 	cout << "2. Deteminant of square matrix" << endl;
 	cout << "3. Multiplication matrix on matrix" << endl;
@@ -97,174 +98,154 @@ int main()
 	cout << "0. Exit" << endl;
 	cout << "Choose variant: ";
 	cin >> Variant;
-	switch(Variant)
-	{
-		case 1:
-		{
-			system("cls");
-			float First_coeff;
-			float Second_coeff;
-			float Third_coeff;
-			float x_0, x_1, x_2;
-			float D;
-			cout << "Input coefficiens of equetion" << endl;
-			// Ввод первого коэффициента, второго коэффициента, третьего коэффициента
-			cout << "First coefficient: "; cin >> First_coeff;
-			cout << "Second coefficient: "; cin >> Second_coeff;
-			cout << "Third coefficient: "; cin >> Third_coeff; 
-			// Вычисление дискриминанта
-			D = Second_coeff * Second_coeff - 4 * First_coeff * Third_coeff; 
-			cout << "\n";
-			cout << "D = " << D << endl;
-			// Проверка на наличие корней уравнения
-			if (D < 0) 
-			{
-				cout << "The discriminant is less than zero. The quadratic equation has no roots" << endl;
-			}
-			else if (D == 0) 
-			{
-				x_0 = (-First_coeff) / 2 * First_coeff; //вычисление единственного корня уравнения
-				cout << "The discriminant is zero. The quadratic equation has 1 real root"<< " " << "x = " << x_0 << endl;
-			}
-			else if (D > 0) 
-			{
-				x_1 = (-Second_coeff + sqrt(D)) / (2 * First_coeff); //вычисление 1-го корня уравнения
-				x_2 = (-Second_coeff - sqrt(D)) / (2 * First_coeff); //вычисление 2-го корня уравнения
-				cout << "The discriminant is greater than zero. The quadratic equation has 2 real roots:" << endl;
-				cout << "x_1 = " << x_1 << endl;
-				cout << "x_2 = " << x_2 << endl;	
-			}
-			break;
-		}
-		case 2:
-		{
-			system("cls");
-		    int Size;
-		    cout << "Input a size of matrix: "; cin >> Size; // Ввод размера квадратной матрицы
-		    int** Matrix = new int*[Size]; //Объявляем матрицу
-		    for (int i = 0; i < Size; i++) 
-			{
-		        Matrix[i] = new int[Size];
-		    }
-		    cout << "\n";
-			cout << "Input a matrix: " << endl; // Ввод элементов матрицы
-		    for (int Lines = 0; Lines < Size; Lines++) 
-			{
-		        for (int Columns = 0; Columns < Size; Columns++) 
-				{
-		        	cout << "Matrix[" << Lines << "][" << Columns << "]";
-		            cin >> Matrix[Lines][Columns]; 
-		        }
-		    }
-		    cout << "\n";
-			cout << "Found determinant: " << Find_Det(Matrix, Size) << endl; // Вывод определителя матрицы
-		    Clear_Memory(Matrix, Size);
-		    break;
-		}
-		case 3:
-		{
-			system("cls");
-			int Matrix1[20][20], Matrix2[20][20];
-			int Lines1, Lines2, Lines3;
-			int Columns1, Columns2, Columns3;
-			int Fin_Matrix[20][20];
-			cout << "Lines and columns of first matrix" << endl; // Ввод размера первой матрицы 
-			cout << "Lines: "; cin >> Lines1;
-			cout << "Columns: "; cin >> Columns1; 
-			cout << "\n";
-			cout << "Input first matrix" << endl; // Ввод элеменов первой матрицы
-			Input_Matrix(Lines1, Columns1, Matrix1);
-			cout << "\n";
-			cout << "First matrix: "<< endl; // Вывод первой матрицы
-			Output_Matrix(Lines1, Columns1, Matrix1);
-			cout << "\n";
-			cout << "Lines and columns of second matrix" << endl; // Ввод второ матрицы
-			cout << "Lines: "; cin >> Lines2;
-			cout << "Columns: "; cin >> Columns2;
-			cout << "\n";
-			cout << "Input second matrix" << endl; // Ввод элеметов второй матрицы
-			Input_Matrix(Lines2, Columns2, Matrix2);
-			cout << "\n";
-			cout << "Second matrix: " << endl; // Вывод второй матрицы
-			Output_Matrix(Lines2, Columns2, Matrix2);
-			system("pause"); 
-			system("cls"); 
-			cout << "First matrix: "<< endl; // Повторный вывод первой матрицы
-			Output_Matrix(Lines1, Columns1, Matrix1);
-			cout << "\n";
-			cout << "Second matrix: " << endl; // Повторный вывод второй матрицы
-			Output_Matrix(Lines2, Columns2, Matrix2);
-			Lines3 = Lines1;
-			Columns3 = Columns2;
-			cout << "\n";
-			if (Columns1 != Lines2) // Проверка на возможность перемножения
-			{
-				cout << "Unreal";
-			}
-			else
-			{
-				cout << "Composition:" << endl; // Произведение матриц
-				Mult_Matrix(Lines1, Lines2, Columns1, Columns2, Matrix1, Matrix2, Fin_Matrix);
-			}
-			break;
-		}
-		case 4:
-		{
-			system("cls");
-			int Lines, Columns;
-			int Matrix[20][20];
-			// Ввод числа строк и столбцов матрицы
-			cout << "Input number of lines: "; cin >> Lines;
-			cout << "Input number of columns: "; cin >> Columns;
-			Input_Matrix(Lines, Columns, Matrix);
-			cout << "\n";
-			cout << "Source matrix:" << endl; // Вывод исходной матрицы
-			Output_Matrix(Lines, Columns, Matrix);
-			cout << "\n";
-			cout << "Transposed matrix:" << endl; // Вывод транспонированной матрицы
-			for (int j = 0; j < Columns; j++)
-			{
-				for (int i = 0; i < Lines; i++)
-				{
-					for (int k = 0; k < Columns; k++)
-					{
-						if (k == j)
-						{
-							Matrix[j][i] = Matrix[i][k];
-							cout.width(7); cout << Matrix[j][i];
-						}
-					}
-				}
-				cout << endl;
-			}
-			break;
-		}
-		case 5:
-		{
-			system("cls");
-			int Lines, Columns, Multiplier;
-			int Matrix[20][20];
-			// Ввод количества строк и столбцов матрицы
-			cout << "Input number of lines: "; cin >> Lines;
-			cout << "Input number of columns: "; cin >> Columns;
-			Input_Matrix(Lines, Columns, Matrix);
-			cout << "\n";
-			cout << "Input multiplier: "; cin >> Multiplier;
-			for (int i = 0; i < Lines; i++)
-			for (int j = 0; j < Columns; j++)
-			{
-				Matrix[i][j] *= Multiplier; // Умножение каждого элемента на число
-			}
-			cout << "\n";
-			cout << "Composed matrix:" << endl;
-			Output_Matrix(Lines, Columns, Matrix); // Вывод произведения матрицы и числа
-			break;
-		}
-		case 0:
-		{
-			system("cls");
-			cout << "Bye :)" << endl;
-			break;
-		}
-	}
+        switch (Variant) {
+            case 1: {
+                float First_coeff;
+                float Second_coeff;
+                float Third_coeff;
+                float x_0, x_1, x_2;
+                float D;
+                cout << "Input coefficiens of equetion" << endl;
+                // Ввод первого коэффициента, второго коэффициента, третьего коэффициента
+                cout << "First coefficient: ";
+                cin >> First_coeff;
+                cout << "Second coefficient: ";
+                cin >> Second_coeff;
+                cout << "Third coefficient: ";
+                cin >> Third_coeff;
+                // Вычисление дискриминанта
+                D = Second_coeff * Second_coeff - 4 * First_coeff * Third_coeff;
+                cout << "\n";
+                cout << "D = " << D << endl;
+                // Проверка на наличие корней уравнения
+                if (D < 0) {
+                    cout << "The discriminant is less than zero. The quadratic equation has no roots" << endl;
+                } else if (D == 0) {
+                    x_0 = (-First_coeff) / 2 * First_coeff; //вычисление единственного корня уравнения
+                    cout << "The discriminant is zero. The quadratic equation has 1 real root" << " " << "x = " << x_0
+                         << endl;
+                } else if (D > 0) {
+                    x_1 = (-Second_coeff + sqrt(D)) / (2 * First_coeff); //вычисление 1-го корня уравнения
+                    x_2 = (-Second_coeff - sqrt(D)) / (2 * First_coeff); //вычисление 2-го корня уравнения
+                    cout << "The discriminant is greater than zero. The quadratic equation has 2 real roots:" << endl;
+                    cout << "x_1 = " << x_1 << endl;
+                    cout << "x_2 = " << x_2 << endl;
+                }
+                break;
+            }
+            case 2: {
+                int Size;
+                cout << "Input a size of matrix: ";
+                cin >> Size; // Ввод размера квадратной матрицы
+                int **Matrix = new int *[Size]; //Объявляем матрицу
+                for (int i = 0; i < Size; i++) {
+                    Matrix[i] = new int[Size];
+                }
+                cout << "\n";
+                cout << "Input a matrix: " << endl; // Ввод элементов матрицы
+                for (int Lines = 0; Lines < Size; Lines++) {
+                    for (int Columns = 0; Columns < Size; Columns++) {
+                        cout << "Matrix[" << Lines << "][" << Columns << "]";
+                        cin >> Matrix[Lines][Columns];
+                    }
+                }
+                cout << "\n";
+                cout << "Found determinant: " << Find_Det(Matrix, Size) << endl; // Вывод определителя матрицы
+                Clear_Memory(Matrix, Size);
+                break;
+            }
+            case 3: {
+                int Matrix1[20][20], Matrix2[20][20];
+                int Lines1, Lines2, Lines3;
+                int Columns1, Columns2, Columns3;
+                int Fin_Matrix[20][20];
+                cout << "Lines and columns of first matrix" << endl; // Ввод размера первой матрицы
+                cout << "Lines: ";
+                cin >> Lines1;
+                cout << "Columns: ";
+                cin >> Columns1;
+                cout << "\n";
+                cout << "Input first matrix" << endl; // Ввод элеменов первой матрицы
+                Input_Matrix(Lines1, Columns1, Matrix1);
+                cout << "\n";
+                cout << "First matrix: " << endl; // Вывод первой матрицы
+                Output_Matrix(Lines1, Columns1, Matrix1);
+                cout << "\n";
+                cout << "Lines and columns of second matrix" << endl; // Ввод второ матрицы
+                cout << "Lines: ";
+                cin >> Lines2;
+                cout << "Columns: ";
+                cin >> Columns2;
+                cout << "\n";
+                cout << "Input second matrix" << endl; // Ввод элеметов второй матрицы
+                Input_Matrix(Lines2, Columns2, Matrix2);
+                cout << "\n";
+                cout << "Second matrix: " << endl; // Вывод второй матрицы
+                Output_Matrix(Lines2, Columns2, Matrix2);
+                cout << "First matrix: " << endl; // Повторный вывод первой матрицы
+                Output_Matrix(Lines1, Columns1, Matrix1);
+                cout << "\n";
+                cout << "Second matrix: " << endl; // Повторный вывод второй матрицы
+                Output_Matrix(Lines2, Columns2, Matrix2);
+                Lines3 = Lines1;
+                Columns3 = Columns2;
+                cout << "\n";
+                if (Columns1 != Lines2) // Проверка на возможность перемножения
+                {
+                    cout << "Unreal"<< endl;
+                } else {
+                    cout << "Composition:" << endl; // Произведение матриц
+                    Mult_Matrix(Lines1, Lines2, Columns1, Columns2, Matrix1, Matrix2, Fin_Matrix);
+                }
+                break;
+            }
+            case 4: {
+                int Lines, Columns;
+                int Matrix[20][20];
+                // Ввод числа строк и столбцов матрицы
+                cout << "Input number of lines: ";
+                cin >> Lines;
+                cout << "Input number of columns: ";
+                cin >> Columns;
+                Input_Matrix(Lines, Columns, Matrix);
+                cout << "\n";
+                cout << "Source matrix:" << endl; // Вывод исходной матрицы
+                Output_Matrix(Lines, Columns, Matrix);
+                cout << "\n";
+                cout << "Transposed matrix:" << endl; // Вывод транспонированной матрицы
+                int mT[20][20];
+                for (int i = 0; i < Lines; i++) {
+                    for (int j = 0; j < Columns; j++) {
+                        mT[j][i] = Matrix[i][j];
+                        }
+                }
+                Output_Matrix(Lines, Columns, mT);
+                break;
+            }
+            case 5: {
+                int Lines, Columns, Multiplier;
+                int Matrix[20][20];
+                // Ввод количества строк и столбцов матрицы
+                cout << "Input number of lines: ";
+                cin >> Lines;
+                cout << "Input number of columns: ";
+                cin >> Columns;
+                Input_Matrix(Lines, Columns, Matrix);
+                cout << "\n";
+                cout << "Input multiplier: ";
+                cin >> Multiplier;
+                for (int i = 0; i < Lines; i++)
+                    for (int j = 0; j < Columns; j++) {
+                        Matrix[i][j] *= Multiplier; // Умножение каждого элемента на число
+                    }
+                cout << "\n";
+                cout << "Composed matrix:" << endl;
+                Output_Matrix(Lines, Columns, Matrix); // Вывод произведения матрицы и числа
+                break;
+            }
+
+
+        }
+    }
+    cout << "Bye :)" << endl;
 }
